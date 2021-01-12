4c3b731d439701ab7a6f1b0f809dac30c7f3f2ad
József Mecsei <mecsei.jozsef@gmail.com>
2021-01-12 13:02:00 +0100

    Initial commit ...

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@Slf4j
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    Consumer<String> ipnMessageConsumer() {
+        return (message) -> {
+            log.info("Got message: {}", message);
+        };
+    }
+}
